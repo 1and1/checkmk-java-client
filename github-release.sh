2dfb55633b869f8011c547d48f2524640d210186
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2022-12-15 10:28:32 +0100

    gh release

---
+++ github-release.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+export SNAPSHOT="false"
+./generate.sh
+mvn -Psonatype-oss-release clean package
+
+source ./settings.sh
+echo "Artifact version: ${ARTIFACT_VERSION}"
+GITTAG="checkmk-java-client-${ARTIFACT_VERSION}"
+echo "Git tag: ${GITTAG}"
+echo "Please confirm"
+read
+git --tag ${GITTAG}
+git push
+git push --tags
+mvn -Psonatype-oss-release deploy
