0a7f7335c0981e1b44e280f43b531869d60fc1ba
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2022-12-16 13:54:25 +0100

    Javadoc

--- demo/src/main/java/com/ionos/network/checkmk/demo/CustomApiClient.java
+++ demo/src/main/java/com/ionos/network/checkmk/demo/CustomApiClient.java
@@ -3,6 +3,11 @@ package com.ionos.network.checkmk.demo;
 import com.ionos.network.checkmk.openapi.ApiClient;
 import com.ionos.network.checkmk.openapi.StringUtil;
 
+/** Since the generated client will only send the accept header for
+ * JSON, Checkmk will refuse this.
+ * This class will make the client accept all acceptable types for the
+ * API endpoint.
+ * */
 public class CustomApiClient extends ApiClient {
     @Override
     public String selectHeaderAccept(String[] accepts) {
